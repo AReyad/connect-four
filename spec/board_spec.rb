5189de841a53d51aa0fdf0f410b6238969e7d1e9
Reyad <20869625+AReyad@users.noreply.github.com>
2026-02-01 07:05:58 +0200

    test(board): add tests to #find_empty_cell_row and #assign_move

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -63,4 +63,42 @@ describe Board do
       end
     end
   end
+
+  describe '#find_empty_cell_row' do
+    context 'when board is empty' do
+      subject(:empty_board) { described_class.new }
+
+      it 'returns the row number with an empty cell within a column' do
+        move = 2
+        column = move - 1 # move - 1 to convert a move which starts at 1 to an array index
+        result = empty_board.find_empty_cell_row(column)
+        expect(result).to eql 5
+      end
+    end
+
+    context 'when column has occupied cells' do
+      subject(:occupied_column_board) { described_class.new([[nil, nil, nil], [nil, 'x', nil], [nil, 'x', nil]]) }
+
+      it 'returns the next row number with an empty cell within a column' do
+        move = 2
+        column = move - 1
+        result = occupied_column_board.find_empty_cell_row(column)
+        expect(result).to eql 0
+      end
+    end
+  end
+
+  describe '#assign_move' do
+    context 'when a move is assigned' do
+      subject(:assign_board) { described_class.new }
+      let(:player) { double('Player') }
+      before do
+        allow(player).to receive(:circle).and_return("\u{1F535}")
+      end
+      it 'places a circle on the board' do
+        move = 5
+        expect { assign_board.assign_move(move, player) }.to(change { assign_board.instance_variable_get(:@board) })
+      end
+    end
+  end
 end
