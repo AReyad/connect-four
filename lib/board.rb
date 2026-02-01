40a1a7adc2461fe937b4c05b320e06e8577275cd
Reyad <20869625+AReyad@users.noreply.github.com>
2026-02-01 07:06:46 +0200

    feat(board): add find_empty_cell_row and assign_move methods

--- lib/board.rb
+++ lib/board.rb
@@ -15,8 +15,19 @@ class Board
     !board[0][move - 1].nil?
   end
 
-  def find_empty_cell(move)
+  def find_empty_cell_row(column)
+    row = board.length - 1
+    row -= 1 until board[row][column].nil?
+    row
   end
+
+  def assign_move(move, player)
+    column = move - 1
+    row = find_empty_cell_row(column)
+    board[row][column] = player.circle
+  end
+
+  private
 
   attr_reader :board
 end
