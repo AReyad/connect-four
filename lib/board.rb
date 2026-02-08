16d68bc0b060846e6bbcd845c419c3fabde7042f
Reyad <20869625+AReyad@users.noreply.github.com>
2026-02-08 11:19:30 +0200

    feat: create win_detection module to handle winning conditions

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,7 @@
 require_relative 'display'
+require_relative 'win_detection'
 class Board
+  include WinDetection
   include Display
   DEFAULT_CELL_VALUE = '  '.freeze
   def initialize(board = Array.new(6) { Array.new(7) { DEFAULT_CELL_VALUE } })
@@ -40,56 +42,7 @@ class Board
 
   private
 
-  def filtered_cords(row, column)
-    cords = (four_column_cords(row, column) + four_row_cords(row, column) +
-          four_diagonal_cords(row, column) + four_diagonal_cords(row, column, -3))
-    cords.reject { |array| array.length < 4 }
-  end
-
-  # returns all possible winning columns from the given position(player's move)
-  def four_column_cords(row, column, acc = 0)
-    positive_set = []
-    negative_set = []
-    4.times do
-      new_column = acc + column
-      positive_set.push([row, new_column]) if valid_column?(new_column)
-      new_column = acc * -1 + column
-      negative_set.push([row, new_column]) if valid_column?(new_column)
-      acc += 1
-    end
-    [positive_set, negative_set]
-  end
-
-  # returns all possible winning rows from the given position(player's move)
-  def four_row_cords(row, column, acc = 0)
-    positive_set = []
-    negative_set = []
-    4.times do
-      new_row = acc + row
-      positive_set.push([new_row, column]) if valid_row?(new_row)
-      new_row = acc * -1 + row
-      negative_set.push([new_row, column]) if valid_row?(new_row)
-      acc += 1
-    end
-    [positive_set, negative_set]
-  end
-
-  # returns all possible winning diagonals from the given position(player's move)
-  def four_diagonal_cords(row, column, acc = 0)
-    main, left, right = Array.new(3) { [] } # creates 3 empty arrays -> main, left, right
-
-    4.times do
-      new_row = acc + row
-      new_column = acc + column
-
-      main.push([new_row, new_column]) if valid_row?(new_row) && valid_column?(new_column)
-      left.push([row - acc, new_column]) if valid_row?(row - acc) && valid_column?(new_column)
-      right.push([new_row, column - acc]) if valid_column?(column - acc) && valid_row?(new_row)
-      acc += 1
-    end
-
-    [main, left, right]
-  end
+  include WinDetection
 
   def valid_row?(row)
     row.between?(0, 5) # 0 lowest row number available, 5 highest row number available within the board
---
+++ lib/win_detection.rb
@@ -0,0 +1,55 @@
+module WinDetection
+  POSSIBLE_WINNING_POSITIONS = [[0, 1, 2, 3], [0, -1, -2, -3], [0, 1, 2, -1], [0, -1, -2, 1]].freeze
+
+  def filtered_cords(row, column)
+    cords = column_cords(row, column) + row_cords(row, column) + diagonal_cords(row, column)
+    cords.reject { |array| array.length < 4 }
+  end
+
+  # returns all possible winning columns from the given position(player's move)
+  def column_cords(row, column)
+    result = []
+    POSSIBLE_WINNING_POSITIONS.each do |positions|
+      cords_array = []
+      positions.each do |position|
+        new_col = position + column
+        cords_array << [row, new_col] if valid_column?(new_col)
+      end
+      result << cords_array
+    end
+    result
+  end
+
+  # returns all possible winning rows from the given position(player's move)
+  def row_cords(row, column)
+    result = []
+    POSSIBLE_WINNING_POSITIONS.each do |positions|
+      cords_array = []
+      positions.each do |position|
+        new_row = position + row
+        cords_array << [new_row, column] if valid_row?(new_row)
+      end
+      result << cords_array
+    end
+    result
+  end
+
+  # returns all possible winning diagonals from the given position(player's move)
+
+  def diagonal_cords(row, column)
+    result = []
+
+    POSSIBLE_WINNING_POSITIONS.each do |positions|
+      main, offset = Array.new(2) { [] } # creates 2 empty arrays -> for main diagonals, and offset diagonals
+      positions.each do |pos|
+        new_row = pos + row
+        new_column = pos + column
+
+        main.push([new_row, new_column]) if valid_row?(new_row) && valid_column?(new_column)
+        offset.push([row - pos, new_column]) if valid_row?(row - pos) && valid_column?(new_column)
+      end
+      result.push(main, offset)
+    end
+    result
+  end
+end
